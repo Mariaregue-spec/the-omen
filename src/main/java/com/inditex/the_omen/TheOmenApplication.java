bf2779ff8a9195e814e81f3e3443348852952984
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-23 12:33:26 +0100

    Initial commit

---
+++ src/main/java/com/inditex/the_omen/TheOmenApplication.java
@@ -0,0 +1,15 @@
+package com.inditex.the_omen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOmenApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(TheOmenApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/inditex/the_omen/TheOmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.the_omen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOmenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
