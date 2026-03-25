5b1617574abf44235d498ace50b6af3c322e74f8
Mariaregue-spec <mariareguecurso@gmail.com>
2026-03-25 14:11:04 +0100

    Feat: add C of CRUD for Movie

--- src/main/java/com/inditex/the_omen/controller/MovieController.java
+++ src/main/java/com/inditex/the_omen/controller/MovieController.java
@@ -2,7 +2,10 @@ package com.inditex.the_omen.controller;
 
 import com.inditex.the_omen.model.Movie;
 import com.inditex.the_omen.repository.MovieRepository;
+import com.inditex.the_omen.service.MovieService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,14 +14,21 @@ import java.util.List;
 public class MovieController {
 
     private final MovieRepository movieRepository;
+    private final MovieService movieService;
 
-    public MovieController(MovieRepository movieRepository) {
+    public MovieController(MovieRepository movieRepository, MovieService movieService) {
         this.movieRepository = movieRepository;
+        this.movieService = movieService;
     }
 
     @GetMapping("/movies")
     public List<Movie> getAllMovies(){
         return movieRepository.findAll();
+    }
+
+    @PostMapping("/movies")
+    public Movie createMovie(@RequestBody Movie newMovie){
+        return movieService.addMovie(newMovie);
     }
 }
 
--- src/main/java/com/inditex/the_omen/service/MovieService.java
+++ src/main/java/com/inditex/the_omen/service/MovieService.java
@@ -1,4 +1,23 @@
 package com.inditex.the_omen.service;
 
+import com.inditex.the_omen.model.Movie;
+import com.inditex.the_omen.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+    private final MovieRepository movieRepository;
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getAll(){
+        return movieRepository.findAll();
+    }
+
+    public Movie addMovie(Movie newMovie){
+        return movieRepository.save(newMovie);
+    }
 }
